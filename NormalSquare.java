f06245b507731fae1c05258fa6c7dc52a81beccb
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-11-09 19:20:12 +0900

    멀쩡한 사각형

---
+++ NormalSquare.java
@@ -0,0 +1,33 @@
+package PROGRAMMERS;
+
+public class NormalSquare {
+    private static class Solution{
+        public long solution(int w, int h) {
+            long answer = 0;
+            long small = h;
+            long big = w;
+            if (w < h) {
+                small = w;
+                big = h;
+            }
+
+            long value = 1;
+            while (value > 0) {
+                value = big % small;
+                big = small;
+                small = value;
+            }
+
+            answer = (long)w * (long)h - ((long)w + (long)h - big);
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(8, 12));
+        System.out.println(sol.solution(8, 8));
+    }
+}
