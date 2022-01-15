84ee4cf47bba3da16fdac13e62b32b76536ced99
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-01-15 20:47:36 +0900

    약수의 합

---
+++ Num12928.java
@@ -0,0 +1,28 @@
+// 코딩테스트 연습 - 연습문제 - 약수의 합
+// https://programmers.co.kr/learn/courses/30/lessons/12928
+
+package PROGRAMMERS.level1;
+
+public class Num12928 {
+    private static class Solution {
+        private int solution(int n) {
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(12));
+        System.out.println(sol.solution(5));
+        System.out.println(sol.solution(0));
+    }
+}
