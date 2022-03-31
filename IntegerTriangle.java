11794f9a78ec65870adbe9a9ab04ca2c2e2cc6f4
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-03-31 22:05:26 +0900

    Update IntegerTriangle.java

--- IntegerTriangle.java
+++ IntegerTriangle.java
@@ -8,57 +8,18 @@ import java.util.Arrays;
 public class IntegerTriangle {
     private static class Solution {
         private int solution(int[][] triangle) {
-            // 테스트
-            for (int t = 0; t < triangle.length; t++) {
-                System.out.println(Arrays.toString(triangle[t]));
-            }
-            System.out.println();
-
             int[][] arr = new int[triangle.length][triangle.length];
-
-            // 테스트
-            for (int t = 0; t < triangle.length; t++) {
-                System.out.println(Arrays.toString(arr[t]));
-            }
-            System.out.println();
 
             // 맨 밑줄 채워주기
             for (int i = 0; i < triangle.length; i++) {
                 arr[arr.length - 1][i] = triangle[triangle.length - 1][i];
             }
 
-            // 테스트
-            for (int t = 0; t < triangle.length; t++) {
-                System.out.println(Arrays.toString(arr[t]));
-            }
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-
-
             for (int i = triangle.length - 2; i >= 0; i--) {
                 for (int j = 0; j <= i; j++) {
                     arr[i][j] = Math.max(triangle[i][j] + arr[i + 1][j], triangle[i][j] + arr[i + 1][j + 1]);
                 }
-
-                // 테스트
-                for (int t = 0; t < triangle.length; t++) {
-                    System.out.println(Arrays.toString(triangle[t]));
-                }
-                System.out.println();
-                // 테스트
-                for (int t = 0; t < triangle.length; t++) {
-                    System.out.println(Arrays.toString(arr[t]));
-                }
-                System.out.println();
-                System.out.println();
-                System.out.println();
-
-
             }
-
-
 
             return arr[0][0];
         }
