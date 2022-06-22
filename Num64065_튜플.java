10da6e4f5a5e2aaf98367bc5b91f25b2e8356bbd
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-06-22 23:43:48 +0900

    Update Num64065_튜플.java

--- Num64065_튜플.java
+++ Num64065_튜플.java
@@ -13,6 +13,8 @@ public class Num64065_튜플 {
             s = s.substring(2, s.length() - 2).replace("},{", "-");
 
             String[] strArray = s.split("-");
+
+            // String 배열을 String.length()의 오름차순으로 정렬!
             Arrays.sort(strArray, Comparator.comparing(String::length));
 
             ArrayList<Integer> arrayList = new ArrayList<>();
@@ -20,9 +22,11 @@ public class Num64065_튜플 {
             for (int i = 0; i < strArray.length; i++) {
                 String str = strArray[i];
                 String[] temp = str.split(",");
+
                 for (int j = 0; j < temp.length; j++) {
                     int tempInt = Integer.parseInt(temp[j]);
-                    if (!arrayList.contains(tempInt)) {
+
+                    if (!arrayList.contains(tempInt)) { // 없는 원소면 추가
                         arrayList.add(tempInt);
                     }
                 }
@@ -41,9 +45,9 @@ public class Num64065_튜플 {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(Arrays.toString(sol.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")));
-//        System.out.println(Arrays.toString(sol.solution("{{1,2,3},{2,1},{1,2,4,3},{2}}")));
-//        System.out.println(Arrays.toString(sol.solution("{{20,111},{111}}")));
-//        System.out.println(Arrays.toString(sol.solution("{{123}}")));
-//        System.out.println(Arrays.toString(sol.solution("{{4,2,3},{3},{2,3,4,1},{2,3}}")));
+        System.out.println(Arrays.toString(sol.solution("{{1,2,3},{2,1},{1,2,4,3},{2}}")));
+        System.out.println(Arrays.toString(sol.solution("{{20,111},{111}}")));
+        System.out.println(Arrays.toString(sol.solution("{{123}}")));
+        System.out.println(Arrays.toString(sol.solution("{{4,2,3},{3},{2,3,4,1},{2,3}}")));
     }
 }
