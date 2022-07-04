f8e1738f95d2f9971ca9a68959f4f658a5caee64
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-07-04 16:29:53 +0900

    신규 아이디 추천

--- Num72410_신규아이디추천.java
+++ Num72410_신규아이디추천.java
@@ -8,9 +8,59 @@ public class Num72410_신규아이디추천 {
         private String solution(String new_id) {
             String answer = new_id;
 
-            answer = answer.toLowerCase();
+            // 1
+            answer = new_id.toLowerCase();
 
+            // 2
+            String id = "";
+            for(int i = 0; i < answer.length(); i++) {
+                char c = answer.charAt(i);
 
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.') {
+                    id+=String.valueOf(c);
+                }
+            }
+            answer = id;
+
+            // 3
+            answer = answer.replace("..", ".");
+            while (answer.contains("..")) {
+                answer = answer.replace("..", ".");
+            }
+
+            // 4
+            if (answer.length() > 0) {
+                if (answer.charAt(0) == '.') {
+                    answer = answer.substring(1, answer.length());
+                }
+            }
+            if (answer.length() > 0) {
+                if (answer.charAt(answer.length() - 1) == '.') {
+                    answer = answer.substring(0, answer.length() - 1);
+                }
+            }
+
+            // 5
+            if (answer.equals("")) {
+                answer = "a";
+            }
+
+            // 6
+            if (answer.length() >= 16) {
+                answer = answer.substring(0, 15);
+
+                if (answer.charAt(answer.length() - 1) == '.') {
+                    answer = answer.substring(0, answer.length() - 1);
+                }
+            }
+
+            // 7
+            String c = answer.charAt(answer.length() - 1) + "";
+            if(answer.length() == 2) {
+                answer = answer + c;
+            } else if(answer.length() == 1) {
+                answer = answer + c + c;
+            }
 
             return answer;
         }
