f6c2c66367d9e850d0062088f7876b21e559e350
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-07-13 23:47:29 +0900

    n진수 게임

---
+++ Num17687_n진수게임.java
@@ -0,0 +1,50 @@
+package PROGRAMMERS.level2;// 2018 KAKAO BLIND RECRUITMENT - [3차] n진수 게임
+// https://programmers.co.kr/learn/courses/30/lessons/17687
+
+public class Num17687_n진수게임 {
+    public static class Solution {
+        public String solution(int n, int t, int m, int p) {
+            String answer = "";
+            StringBuilder sb = new StringBuilder();
+
+            int i = 1;
+            sb.append(0);
+            while (sb.length() < m * t) {
+                sb.append(nBinary(i++, n));
+            }
+
+            for (int j = p - 1; j < m * (t - 1) + p; j += m) {
+                answer += sb.charAt(j);
+            }
+
+
+            return answer;
+        }
+
+        public String nBinary(int num, int n) {
+            String str = "";
+            while (num != 0) {
+
+                if (num % n < 10) { //10진수 이하인 경우
+                    str = num % n + str;
+                } else {    //10진수 이상인 경우
+                    str = String.valueOf((char) (num % n + 'A' - 10)) + str;
+                }
+                num = num / n;
+            }
+
+            return str;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.solution(2, 4, 2, 1));
+
+        System.out.println();
+
+        System.out.println(s.solution(16, 16, 2, 1));
+    }
+}
