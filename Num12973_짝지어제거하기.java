f2fa03a4cc3a5a2f1836d6370f815b53ba27744f
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-07-20 23:02:03 +0900

    짝지어 제거하기

---
+++ Num12973_짝지어제거하기.java
@@ -0,0 +1,33 @@
+package PROGRAMMERS.level2;
+
+import java.util.Stack;
+
+public class Num12973_짝지어제거하기 {
+    public static class Solution {
+        public int solution(String s) {
+            Stack<Character> stack = new Stack<Character>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (!stack.isEmpty() && stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.solution("baabaa"));
+        System.out.println(s.solution("cdcd"));
+    }
+}
