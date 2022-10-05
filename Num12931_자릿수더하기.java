432ecb1f1eac493c75d88c7e7ebf78d779e52529
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2022-10-05 23:55:27 +0900

    자릿수 더하기

---
+++ Num12931_자릿수더하기.java
@@ -0,0 +1,26 @@
+// 코딩테스트 연습 - 연습문제 - 자릿수 더하기
+// https://programmers.co.kr/learn/courses/30/lessons/12931
+
+package PROGRAMMERS.level1;
+
+public class Num12931_자릿수더하기 {
+    private static class Solution {
+        private int solution(int n) {
+            int sum = 0;
+
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(123));
+        System.out.println(sol.solution(987));
+    }
+}
