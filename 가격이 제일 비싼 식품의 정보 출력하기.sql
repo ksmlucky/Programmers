8af7e99c56d23c8c9044f308b06eb5e065d67355
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2023-02-22 09:55:44 +0900

    가격이 제일 비싼 식품의 정보 출력하기

---
+++ 가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,17 @@
+-- 서브쿼리
+SELECT
+    PRODUCT_ID,
+    PRODUCT_NAME,
+    PRODUCT_CD,
+    CATEGORY,
+    PRICE
+FROM
+    FOOD_PRODUCT
+WHERE
+    PRICE = 
+        (
+        SELECT
+            MAX(PRICE)
+        FROM
+            FOOD_PRODUCT
+        )
