c747d33126f2156b2c345cd0b4bd54c762fbb375
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2023-03-10 10:43:45 +0900

    Create 문자열 내 p와 y의 개수.py

---
+++ Python/문자열 내 p와 y의 개수.py
@@ -0,0 +1,15 @@
+def solution(s):
+    s = s.lower()
+    pCount = 0
+    yCount = 0
+
+    for i in s:
+        if i == 'p':
+            pCount += 1
+        elif i == 'y':
+            yCount += 1
+
+    if pCount == yCount:
+        return True
+    else:
+        return False
