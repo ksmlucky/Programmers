48420bd36ad4883ac0ced1946749e8854815ac55
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2023-04-09 22:38:31 +0900

    Create 즐겨찾기가 가장 많은 식당의 정보 출력하기.sql

---
+++ MySQL/즐겨찾기가 가장 많은 식당의 정보 출력하기.sql
@@ -0,0 +1,23 @@
+-- 서브쿼리
+-- (A, B, ...) IN (a, b, ...)
+SELECT
+    FOOD_TYPE
+    , REST_ID
+    , REST_NAME
+    , FAVORITES
+FROM
+    REST_INFO
+WHERE
+    (FOOD_TYPE, FAVORITES)
+    IN
+        (
+            SELECT
+                FOOD_TYPE, MAX(FAVORITES)
+            FROM
+                REST_INFO
+            GROUP BY
+                FOOD_TYPE
+        )
+ORDER BY
+    FOOD_TYPE
+    DESC
