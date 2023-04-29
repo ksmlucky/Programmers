84dc2a550bbede4e8fe8561dc795a89e4d1b86e9
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2023-04-30 00:51:01 +0900

    Create 소수 찾기.py

---
+++ Python/소수 찾기.py
@@ -0,0 +1,22 @@
+# import math
+#
+# def solution(n):
+#     answer = 0
+#
+#     for i in range(2, n + 1):
+#         for j in range(2, int(math.sqrt(i)) + 1):
+#             if i % j == 0:
+#                 break
+#         else:
+#             answer += 1
+#
+#     return answer
+
+def solution(n):
+    num = set(range(2, n + 1))
+
+    for i in range(2, n + 1):
+        if i in num:
+            num -= set(range(2 * i, n + 1, i))
+
+    return len(num)
